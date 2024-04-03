980343b970530dde23b310a2484ba9c923b773c7
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-04-03 10:58:04 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+// console.log(refs.btnStart);
+// console.log(refs.btnStop);
+// console.log(refs.body);
+let timerId = null;
+
+function ifClickOnBtn() {
+    if(refs.btnStart !== disabled) {
+        onBtnStartClick();
+    }
+
+}
+
+
+const onBtnStartClick = () => {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+        
+    }, 1000);
+    console.log("Start");
+  };
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+  
+
+const onBtnStopClick = () => {
+    clearInterval(timerId);
+    console.log("Stop")
+    
+  };
+
+
+refs.btnStart.addEventListener("click", onBtnStartClick);
+refs.btnStop.addEventListener("click", onBtnStopClick);
 

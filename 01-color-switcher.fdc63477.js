b5291bb9e7365115d10d64eb8b776b8d0dd7d3b7
Natalia2110 <119740688+Natalia2110@users.noreply.github.com>
2024-04-03 08:52:57 +0000

    Deploying to gh-pages from @ Natalia2110/goit-js-hw-09@21bc37b7e0126761da6cf17a1d677656b4b0c971 🚀

--- 01-color-switcher.fdc63477.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")},o=null;var n=function(){o=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),console.log("Start")};t.btnStart.addEventListener("click",n),t.btnStop.addEventListener("click",(function(){clearInterval(o),console.log("Stop")}))}();
-//# sourceMappingURL=01-color-switcher.fdc63477.js.map
